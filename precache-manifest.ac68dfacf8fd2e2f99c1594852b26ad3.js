7007d02a87a6184b29e728145c38c77437c26330
klemminn <klemenz88@gmail.com>
2020-10-14 00:55:32 +0000

    Updates

---
+++ precache-manifest.ac68dfacf8fd2e2f99c1594852b26ad3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091ea12b0aeecd0432ce9e2b9bbe20ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfbc517700a2e8d0023c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5b0d9b6775643df6cddd",
+    "url": "/static/css/main.6fa638e0.chunk.css"
+  },
+  {
+    "revision": "bfbc517700a2e8d0023c",
+    "url": "/static/js/2.ddf3c528.chunk.js"
+  },
+  {
+    "revision": "a69bc0090a30f5e92341104b9e2aed7e",
+    "url": "/static/js/2.ddf3c528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0d9b6775643df6cddd",
+    "url": "/static/js/main.8efeeae5.chunk.js"
+  },
+  {
+    "revision": "5eabe38bc174b5df22e6",
+    "url": "/static/js/runtime-main.53fdc8f4.js"
+  },
+  {
+    "revision": "0309df2857cdc4fc3bb9a31565633e38",
+    "url": "/static/media/vaktinlogo.0309df28.png"
+  }
+]);
