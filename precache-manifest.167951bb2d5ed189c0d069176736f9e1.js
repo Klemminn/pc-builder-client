722bc5b0f2a300370807fc838da2e28cb7628f71
klemminn <klemenz88@gmail.com>
2021-03-15 20:04:12 +0000

    Updates

---
+++ precache-manifest.167951bb2d5ed189c0d069176736f9e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43abc17e693c0a438cff6ed0c184bbd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3bd5bea8e17532c2ab6",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a459dac02562ec45469c",
+    "url": "/static/css/main.15371a23.chunk.css"
+  },
+  {
+    "revision": "e3bd5bea8e17532c2ab6",
+    "url": "/static/js/2.0a6a4781.chunk.js"
+  },
+  {
+    "revision": "a69bc0090a30f5e92341104b9e2aed7e",
+    "url": "/static/js/2.0a6a4781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a459dac02562ec45469c",
+    "url": "/static/js/main.c8cbbe85.chunk.js"
+  },
+  {
+    "revision": "5eabe38bc174b5df22e6",
+    "url": "/static/js/runtime-main.53fdc8f4.js"
+  },
+  {
+    "revision": "0309df2857cdc4fc3bb9a31565633e38",
+    "url": "/static/media/vaktinlogo.0309df28.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f2d4b707bcf4906ef1d406a447b47a4.js"
+  "/precache-manifest.167951bb2d5ed189c0d069176736f9e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
