87507089bdb69aceb02fc8b3bfabcb9acdc99d45
Viktor.ms <vjh2@hi.is>
2022-07-28 20:46:11 +0000

    Fix history push compact

--- src/components/BuildPageCompact.tsx
+++ src/components/BuildPageCompact.tsx
@@ -83,7 +83,7 @@ const HomePage: React.FC = () => {
         }
       }
       if (currentBuild?.buildId) {
-        history.push(`/build/${currentBuild?.buildId}`);
+        history.push(`/build/${currentBuild?.buildId}/compact`);
       }
       buildState.set({ ...BuildState.defaultBuild, ...currentBuild });
     };
